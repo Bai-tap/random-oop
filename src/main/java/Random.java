ab5c857c0e1a4ea2add6871efa905e788fe4b8dc
longhoang94 <longsutls@gmail.com>
2018-08-28 10:25:25 +0700

    add application Random

---
+++ src/main/java/Random.java
@@ -0,0 +1,7 @@
+public class Random {
+    public static void main(String[] args) {
+        MyRandom myRandom = new MyRandom();
+        myRandom.getRandomNumber();
+        myRandom.displayNumber();
+    }
+}
